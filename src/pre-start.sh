8fdd2dab1915184df4a89e9d516dea4d2750a630
Axel Spenneberg <a.spenneberg@sap.com>
2022-02-08 17:24:34 +0100

    update

--- src/pre-start.sh
+++ src/pre-start.sh
@@ -2,19 +2,28 @@
 export NUMBER="${NUMBER:-"undef"}"
 export SLEEPTIME="${SLEEPTIME:-"10s"}"
 export LIRC="${LIRC:-"no"}"
+SIGNALUSER=signal-cli
 
 if [ $NUMBER != "undef" ]; then
+
+   echo -n "Checking user $SIGNALUSER ..."
+	if id "$SIGNALUSER" &>/dev/null; then
+	    echo 'found'
+	else
+		adduser --disabled-password --gecos none $SIGNALUSER
+	    echo 'created'
+	fi
 
    if [ -s /var/run/dbus/pid ]; then
       rm /var/run/dbus/pid
    fi
 
    echo "Starting DBus System Daemon"
-   dbus-daemon --system --address=unix:path=/run/dbus/system_bus_socket
+   dbus-daemon --system --address=unix:path=/run/dbus/system_bus_socket &
    
    echo $NUMBER
    echo "Starting signal-cli"
-   sudo -u fhem /usr/local/bin/signal-cli -u $NUMBER --config /opt/fhem/.local/share/signal-cli/ daemon --system &
+   sudo -u signal-cli /usr/local/bin/signal-cli -u $NUMBER --config /opt/fhem/.local/share/signal-cli/ daemon --system &
 
    echo "Wait $SLEEPTIME to give signal-cli time to come up"
    sleep $SLEEPTIME
